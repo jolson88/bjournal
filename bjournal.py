4971b29de3df324f719b1dea0bde4fd2c464b4c7
Jason Olson <jolson88@outlook.com>
2022-02-22 19:05:46 -0800

    Basic dataclass

--- bjournal.py
+++ bjournal.py
@@ -1,5 +1,18 @@
+from dataclasses import dataclass
+from datetime import date
+
+@dataclass
+class Task:
+	name: str
+	date: date
+	status: str
+
 def main():
 	print("Welcome to bjournal, a programmer's bullet journal.")
+	print("Today's date is", date.today().isoformat())
+
+	task = Task("My first task", date(2022, 2, 21), "Completed")
+	print(task)
 
 if __name__ == "__main__":
 	main()
