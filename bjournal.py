58239d2bfe6b2c45f85b49dfa8e10cec7a6e9f1e
Jason Olson <jolson88@outlook.com>
2022-02-26 12:38:44 -0800

    Basic command processing structure and REPL

--- bjournal.py
+++ bjournal.py
@@ -7,12 +7,51 @@ class Task:
 	date: date
 	status: str
 
+@dataclass
+class CommandContext:
+	originalInput: str
+	command: str
+	argumentString: str
+
+def newTaskCommand(ctx: CommandContext):
+	tasks.append(Task(ctx.argumentString, date.today(), "NotStarted"))
+
+def quitCommand(_ctx: CommandContext):
+	print("Thanks for using bjournal!")
+	exit(0)
+
+def parseInput(userInput: str) -> CommandContext:
+	userInput = userInput + " " # Somewhat of a "hack" to make it easier to parse out command and arguments
+	userCommand = userInput[userInput.find(" "):]
+	return CommandContext(
+		originalInput = userInput.strip(),
+		command = userInput[:userInput.find(" ")],
+		argumentString = userInput[userInput.find(" "):].strip()
+	)
+
+commands = {
+	"n": newTaskCommand,
+	"q": quitCommand,
+	"quit": quitCommand,
+}
+tasks = []
+
 def main():
 	print("Welcome to bjournal, a programmer's bullet journal.")
 	print("Today's date is", date.today().isoformat())
+	
+	while True:
+		userInput = input("? ")
+		if userInput.strip() == "":
+			print("Please enter a command")
+			continue
 
-	task = Task("My first task", date(2022, 2, 21), "Completed")
-	print(task)
+		commandContext = parseInput(userInput);
+		command = commands.get(commandContext.command)
+		if (command is not None):
+			command(commandContext)
+		else:
+			print(f'Command "{commandContext.command}" is not recognized!')
 
 if __name__ == "__main__":
 	main()
